e51537560a27b9df51444e313215b8a1b4100e2f
Steve Liedtke <sliedtke57@gmail.com>
2013-03-03 13:42:20 +0100

    removed header in own file

--- index.php
+++ index.php
@@ -21,31 +21,9 @@ Bremen, entwerfen, programmieren, löten, bauen, Workshops, Vorträge, Kunst, Ha
 
       <div id="container">
 
-<!--- Kopfzeile -->
-
-         <div id="head">
-            <table width=960 height=27 border=0 cellspacing="0" cellpadding="0" background="#top.png" >
-               <tr>
-                  <td width=200></td>
-                  <td align=left>
-                     <ul>
-                        <li><a href="http://www.hackerspace-bremen.de">Startseite</a></li>
-                        <li>|</li>
-                        <li><a href="https://chili.hackerspace-bremen.de/projects/web/wiki/%C3%9Cber_uns">Über uns</a></li>
-                        <li>|</li>
-                        <li><a href="https://chili.hackerspace-bremen.de/projects/web/wiki/Location">Location</a></li>
-                        <li>|</li>
-                        <li><a href="https://chili.hackerspace-bremen.de/projects">Projekte</a></li>
-                        <li>|</li>
-                        <li><a href="https://chili.hackerspace-bremen.de/projects/web/wiki/Regelm%C3%A4%C3%9Fige_Termine"> Veranstaltungen </a></li>
-						<li>|</li>
-                        <li><a href="https://chili.hackerspace-bremen.de/projects/web/wiki/Sponsoren"> Sponsoren </a></li>
-                     </ul>
-                  </td>
-                  <td align=right><div id="impressum"><a href="https://chili.hackerspace-bremen.de/projects/web/wiki/Impressum">Impressum</font></a></div></td>
-               </tr>
-            </table>
-         </div>
+		<?php
+			include 'basicPhps/header.php';
+		?>
 
 <!-- linke Seitenleiste -->
 

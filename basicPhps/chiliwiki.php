92e72a533965847661f4635af8912a90cdec77ba
Steve Liedtke <sliedtke57@gmail.com>
2013-03-15 18:37:04 +0100

    outsourced the chili content

---
+++ basicPhps/chiliwiki.php
@@ -0,0 +1,17 @@
+<?php
+	header("Content-Type: text/html; charset=utf-8"); 
+	$dom = new DOMDocument('1.0','UTF-8');
+	//$profile->file_get_contents('https://chili.hackerspace-bremen.de/projects/web/wiki/Sponsoren');
+	//$dom->loadHTML(mb_convert_encoding($profile, 'HTML-ENTITIES', 'UTF-8'));
+	$dom->loadHTMLFile($chiliurl);
+	$dom->encoding = 'UTF-8';
+	$xpath = new DOMXPath($dom);
+	$elements = $xpath->query('//div[contains(attribute::class, "wiki")]');
+	//get all content
+	foreach( $elements as $element ) {
+		$newdoc = new DOMDocument();
+		$cloned = $element->cloneNode(TRUE);
+		$newdoc->appendChild($newdoc->importNode($cloned,TRUE));
+		echo $newdoc->saveHTML();
+	}
+?>
--- sponsors.php
+++ sponsors.php
@@ -23,22 +23,9 @@
 				<div id="content">
 					<!-- Sponsors-->  
 					<?php
-						header("Content-Type: text/html; charset=utf-8"); 
-						$dom = new DOMDocument('1.0','UTF-8');
-						//$profile->file_get_contents('https://chili.hackerspace-bremen.de/projects/web/wiki/Sponsoren');
-						//$dom->loadHTML(mb_convert_encoding($profile, 'HTML-ENTITIES', 'UTF-8'));
-						$dom->loadHTMLFile('https://chili.hackerspace-bremen.de/projects/web/wiki/Sponsoren');
-						$dom->encoding = 'UTF-8';
-						$xpath = new DOMXPath($dom);
-						$elements = $xpath->query('//div[contains(attribute::class, "wiki")]');
-						//get all content
-						foreach( $elements as $element ) {
-							$newdoc = new DOMDocument();
-							$cloned = $element->cloneNode(TRUE);
-							$newdoc->appendChild($newdoc->importNode($cloned,TRUE));
-							echo $newdoc->saveHTML();
-						}
+						$chiliurl = 'https://chili.hackerspace-bremen.de/projects/web/wiki/Sponsoren';
 					?>
+					<? include ("basicPhps/chiliwiki.php"); ?>
 				</div>
 				<? include ("basicPhps/rightside.php"); ?>
 				<? include ("basicPhps/footer.php"); ?>

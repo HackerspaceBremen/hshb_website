583f3a2b21f2b458d5d8134e23f5c3856d235176
Steve Liedtke <sliedtke57@gmail.com>
2013-03-15 20:08:52 +0100

    changed the theme a little bit

--- basicPhps/leftside.php
+++ basicPhps/leftside.php
@@ -2,14 +2,15 @@
 
 <div id="sidebar-1">
 
-	<img src="logo.png" width=180></img>
+	<img src="logo.png"></img>
 
 	<!--<a href="http://www.hackerspace-bremen.de" target=_blank><h1>Newsletter (bald)</h1></a>-->
 
-	<h1>Follow on</h1>
+	<h1>Folge uns auf: </h1>
 	<ul>
 		<li><a href="http://www.facebook.com/pages/Hackerspace-Bremen/217887601561428?ref=ts" target=_blank>Facebook</a></li>
-		<li><a href="https://plus.google.com/106849621647585475724" target=_blank>Google+</a></li>
+		<li><a href="https://plus.google.com/106849621647585475724" target=_blank rel="publisher">Google+</a></li>
+		<li><a href="https://twitter.com/hspacehb" target=_blank>Twitter</a></li>
 	</ul>
 	<br /><a href="HackerspaceBremen-Flyer.pdf" target=_blank><img src=flyer.jpg width=80></img><p style="margin-top:5px;">Download Flyer (PDF)</p></a>
 	<br />

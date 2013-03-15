527af003b78f76925f06fc05fe1d34a0e26cb697
Steve Liedtke <sliedtke57@gmail.com>
2013-03-15 21:13:38 +0100

    made little changes

--- basicPhps/leftside.php
+++ basicPhps/leftside.php
@@ -12,7 +12,7 @@
 		<li><a href="https://plus.google.com/106849621647585475724" target=_blank rel="publisher">Google+</a></li>
 		<li><a href="https://twitter.com/hspacehb" target=_blank>Twitter</a></li>
 	</ul>
-	<br /><a href="HackerspaceBremen-Flyer.pdf" target=_blank><img src=flyer.jpg width=80></img><p style="margin-top:5px;">Download Flyer (PDF)</p></a>
+	<br /><a href="HackerspaceBremen-Flyer.pdf" target=_blank><img src="flyer.jpg"></img><p style="margin-top:5px;">Download Flyer (PDF)</p></a>
 	<br />
 	<br /><a href="https://chili.hackerspace-bremen.de/attachments/download/5/Satzung.pdf" target=_blank>Download Satzung (PDF)</a>
 	<br />

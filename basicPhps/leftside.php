6de9af48981ebe80efe810b1ef4b42199c8886e8
Steve Liedtke <sliedtke57@gmail.com>
2013-03-19 12:36:37 +0100

    added google+ badge

--- basicPhps/leftside.php
+++ basicPhps/leftside.php
@@ -6,9 +6,13 @@
 
 	<h1>Folge uns auf: </h1>
 	<div class="fb-follow" data-href="https://www.facebook.com/pages/Hackerspace-Bremen/217887601561428" data-layout="box_count" data-show-faces="false" data-font="arial" data-width="60"></div>
+	<!-- Fügen Sie diesen Code dort ein, wo die Box dargestellt werden soll. -->
+	<a href="//plus.google.com/106849621647585475724?prsrc=3" rel="publisher" style="text-decoration:none;display:inline-block;color:#333;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
+	<img src="//ssl.gstatic.com/images/icons/gplus-64.png" alt="" style="border:0;width:64px;height:64px;"/><br />
+	<span style="font-weight:bold;">Folge uns auf Google+</span><br /><span>auf Google+</span></a>
 	<ul>
 		<!--<li><a href="http://www.facebook.com/pages/Hackerspace-Bremen/217887601561428?ref=ts" target=_blank>Facebook</a></li>-->
-		<li><a href="https://plus.google.com/106849621647585475724" target=_blank rel="publisher">Google+</a></li>
+		<!--<li><a href="https://plus.google.com/106849621647585475724" target=_blank rel="publisher">Google+</a></li>-->
 		<li><a href="https://twitter.com/hspacehb" target=_blank>Twitter</a></li>
 	</ul>
 	

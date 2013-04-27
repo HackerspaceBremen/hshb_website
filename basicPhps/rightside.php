0126cb4707b0e5f6d42e0500fbd1b376398314d1
Daniel Wendt-Fröhlich <daniel.w-froehlich@arcor.de>
2013-04-27 12:53:30 +0300

    Update rightside.php
    updated dates

--- basicPhps/rightside.php
+++ basicPhps/rightside.php
@@ -37,8 +37,7 @@
 				<br /><br />Dienstags, ab 19:00 <br>&rarr;<b>Öffentlicher Themenabend</b>
 				<br /><br />Mittwochs, ab 17:00 <br>&rarr;<b>Roboter bauen und programmieren</b>
 				<br /><br />Mittwochs, ab 19:00 <br>&rarr;<b>eve(ning) of projects</b>
-				<br /><br />Donnerstags, ab 19:00 <br>&rarr;<b>Rapid Prototyping (3D-Druck Abend)</b>
-				<br /><br />Freitags, ab 19:00 <br>&rarr;<b>"notopic"</b>
+				<br /><br />Donnerstags, ab 18:00 <br>&rarr;<b>Rapid Prototyping (3D-Druck Abend)</b>
 			</td>
 		</tr>
 	</table>

542a635a43ebff7b30fc6af6c3a695440aba5f4e
Daniel Wendt-Fröhlich <daniel.w-froehlich@arcor.de>
2013-05-13 09:19:14 +0200

    Update rightside.php
    added date

--- basicPhps/rightside.php
+++ basicPhps/rightside.php
@@ -38,7 +38,7 @@
 				<br /><br />Mittwochs, ab 17:00 <br>&rarr;<b>Roboter bauen und programmieren</b>
 				<br /><br />Mittwochs, ab 19:00 <br>&rarr;<b>eve(ning) of projects</b>
 				<br /><br />Donnerstags, ab 19:00 <br>&rarr;<b>Rapid Prototyping (3D-Druck Abend)</b>
-				
+				<br /><br />Sonntag, ab 19:00 <br>&rarr;<b>Linux-User-Group-Bremen Stammtisch</b>
 			</td>
 		</tr>
 	</table>
